114395403b5820328a5caf41581f0559a993afb0
kyzaiv <kyzaiv1@yandex.ru>
2022-06-20 00:58:10 +1000

    Initial commit

---
+++ src/main/java/com/example/schoolsample/SchoolsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.schoolsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolsampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schoolsample/SchoolsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolsampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
